172fcbc628ae49c202f548d08a95665f62c44371
AbelBusi <cesarabelbustamante@gmail.com>
2026-04-16 18:55:29 -0500

    feat: implementacion de la entidad HorarioAtencion

--- consultorio/src/main/java/com/salud/consultorio/model/HorarioAtencion.java
+++ consultorio/src/main/java/com/salud/consultorio/model/HorarioAtencion.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalTime;
 import java.util.List;
 
 
@@ -21,7 +22,18 @@ public class HorarioAtencion {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @OneToOne
+    @JoinColumn(name = "id_doctor", nullable = false)
+    private Doctor doctor;
 
+    @Column(name = "dia_semana",nullable = false)
+    private Integer diaSemana;
+
+    @Column(name = "hora_entrada",nullable = false)
+    private LocalTime horaEntrada;
+
+    @Column(name = "hora_salida",nullable = false)
+    private LocalTime horaSalida;
 
     @Column(name = "estado", nullable = false)
     private Integer estado;
