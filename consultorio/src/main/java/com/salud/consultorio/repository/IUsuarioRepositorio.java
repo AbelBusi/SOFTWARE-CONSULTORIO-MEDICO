af9b0cf35b5679b0332152fc1cd688c1fb6d19e9
AbelBusi <cesarabelbustamante@gmail.com>
2026-05-12 22:08:43 -0500

    feat: Agregar repositorios de usuario y rol

---
+++ consultorio/src/main/java/com/salud/consultorio/repository/IUsuarioRepositorio.java
@@ -0,0 +1,9 @@
+package com.salud.consultorio.repository;
+
+import com.salud.consultorio.model.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUsuarioRepositorio extends JpaRepository<Usuario, Integer> {
+}
