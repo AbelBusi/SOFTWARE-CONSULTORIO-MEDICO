eff0d56211b25becb7d118f91143717928a75e92
AbelBusi <cesarabelbustamante@gmail.com>
2026-05-14 22:58:06 -0500

    feat: validar token y cerrar sesion de usuario

---
+++ consultorio/src/main/java/com/salud/consultorio/auth/configuration/JwtAuthFilter.java
@@ -0,0 +1,106 @@
+package com.salud.consultorio.auth.configuration;
+
+import com.salud.consultorio.auth.service.IJwtServicio;
+import com.salud.consultorio.model.entity.Token;
+import com.salud.consultorio.model.entity.Usuario;
+import com.salud.consultorio.repository.ITokenRepositorio;
+import com.salud.consultorio.repository.IUsuarioRepositorio;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final IJwtServicio jwtServicio;
+    private final UserDetailsService userDetailsService;
+    private final ITokenRepositorio tokenRepositorio;
+    private final IUsuarioRepositorio usuarioRepositorio;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+        if (request.getServletPath().contains("/auth")){
+
+            filterChain.doFilter(request,response);
+
+            return;
+
+        }
+
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")){
+
+            filterChain.doFilter(request,response);
+
+        }
+
+        final String jwtToken = authHeader.substring(7);
+        final String usuario = jwtServicio.extraerUsuario(jwtToken);
+
+        if (usuario == null || SecurityContextHolder.getContext().getAuthentication() != null){
+
+            return;
+
+        }
+
+        final Token token = tokenRepositorio.findByToken(jwtToken).orElseThrow(
+                ()-> new UsernameNotFoundException("Token Bearer vacio")
+        );
+
+        if (token == null || token.isExpired() || token.isRevoked()){
+
+            filterChain.doFilter(request,response);
+            return;
+
+        }
+
+        final UserDetails userDetails = this.userDetailsService.loadUserByUsername(usuario);
+        final Optional<Usuario> user = usuarioRepositorio.findByUsuario(userDetails.getUsername());
+
+        if (user.isEmpty()){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        final boolean isTokenValidado = jwtServicio.tokenValido(jwtToken,user.get());
+
+        if (!isTokenValidado){
+            return;
+        }
+
+        final var authToken = new UsernamePasswordAuthenticationToken(
+                userDetails,
+                null,
+                userDetails.getAuthorities()
+        );
+
+        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        filterChain.doFilter(request,response);
+
+    }
+
+
+}
--- consultorio/src/main/java/com/salud/consultorio/configuration/SecurityFilterConfig.java
+++ consultorio/src/main/java/com/salud/consultorio/configuration/SecurityFilterConfig.java
@@ -1,17 +1,29 @@
 package com.salud.consultorio.configuration;
 
+import com.salud.consultorio.auth.configuration.JwtAuthFilter;
+import com.salud.consultorio.model.entity.Token;
+import com.salud.consultorio.repository.ITokenRepositorio;
+import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityFilterConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+    private final ITokenRepositorio tokenRepositorio;
 
     @Bean
     public SecurityFilterChain securityFilterChain(@NonNull HttpSecurity http) throws Exception {
@@ -23,9 +35,34 @@ public class SecurityFilterConfig {
                         .requestMatchers("/api/v1/pacientes/**").permitAll()
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .anyRequest().authenticated()
-                ).formLogin(Customizer.withDefaults());
+                ).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout(logout ->
+                        logout.logoutUrl("/auth/logout")
+                                .addLogoutHandler((request, response, authentication) -> {
+                                    final var authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+                                    logout(authHeader);
+                                }));
 
         return http.build();
+
+    }
+
+    private void logout(final String token){
+
+        if (token == null || !token.startsWith("Bearer ")){
+
+            throw new IllegalArgumentException("Token Invalido");
+
+        }
+
+        final String jwtToken = token.substring(7);
+        final Token foundToken = tokenRepositorio.findByToken(jwtToken)
+                .orElseThrow(()-> new IllegalArgumentException("Invalid Token"));
+        foundToken.setExpired(true);
+        foundToken.setRevoked(true);
+        tokenRepositorio.save(foundToken);
 
     }
 
