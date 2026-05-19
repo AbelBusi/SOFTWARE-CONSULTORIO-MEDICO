41e8e4310b71c298b8bff68ccd4d8beb5eae27a5
AbigaylMC <abimunozcoben16@gmail.com>
2026-05-18 20:12:54 -0500

    actualizacion

--- consultorio/src/main/java/com/salud/consultorio/dto/doctor/EspecialidadRefDoctorDTO.java
+++ consultorio/src/main/java/com/salud/consultorio/dto/doctor/EspecialidadRefDoctorDTO.java
@@ -1,15 +1,21 @@
 package com.salud.consultorio.dto.doctor;
 
-import jakarta.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Schema(description = "DTO de referencia de especialidad para el doctor")
 public class EspecialidadRefDoctorDTO {
 
     @NotNull(message = "El id de la especialidad es obligatorio")
+    @Schema(
+            description = "Identificador único de la especialidad médica",
+            example = "4"
+    )
     private Integer id;
 
 }
