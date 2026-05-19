86ee6a6842cbee06529736578cd82cf1072e0c0b
AbigaylMC <abimunozcoben16@gmail.com>
2026-05-19 09:53:23 -0500

    actualizacion

--- consultorio/src/main/java/com/salud/consultorio/dto/doctor/NombreDoctoresDTO.java
+++ consultorio/src/main/java/com/salud/consultorio/dto/doctor/NombreDoctoresDTO.java
@@ -1,5 +1,6 @@
 package com.salud.consultorio.dto.doctor;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -11,10 +12,20 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "DTO de nombres de doctores")
 public class NombreDoctoresDTO {
+
     @NotNull(message = "El id del doctor es obligatorio")
+    @Schema(
+            description = "Identificador único del doctor",
+            example = "3"
+    )
     private Integer idDoctor;
 
     @NotBlank(message = "El nombre del doctor es obligatorio")
+    @Schema(
+            description = "Nombre completo del doctor",
+            example = "María Fernández"
+    )
     private String nombreDoctor;
 }
