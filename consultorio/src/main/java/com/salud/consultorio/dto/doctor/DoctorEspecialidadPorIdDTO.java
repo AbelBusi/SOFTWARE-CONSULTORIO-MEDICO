b5392225af0335866e5ab98f3e840a746393bdff
AbigaylMC <abimunozcoben16@gmail.com>
2026-05-19 09:50:56 -0500

    actualizacion

--- consultorio/src/main/java/com/salud/consultorio/dto/doctor/DoctorEspecialidadPorIdDTO.java
+++ consultorio/src/main/java/com/salud/consultorio/dto/doctor/DoctorEspecialidadPorIdDTO.java
@@ -1,5 +1,6 @@
 package com.salud.consultorio.dto.doctor;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,10 +8,19 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
-public class DoctorEspecialidadPorIdDTO{
+@Schema(description = "DTO de lectura de doctor por identificador")
+public class DoctorEspecialidadPorIdDTO {
 
+    @Schema(
+            description = "Identificador único del doctor",
+            example = "2"
+    )
     private Integer id;
 
+    @Schema(
+            description = "Nombre completo del doctor",
+            example = "Juan Pérez"
+    )
     private String nombres;
 
     public DoctorEspecialidadPorIdDTO(Integer id, String nombres) {
