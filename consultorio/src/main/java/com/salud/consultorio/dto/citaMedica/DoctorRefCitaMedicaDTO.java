1e8b27f371ebf33cfee6b40d92124657f3756740
AbigaylMC <abimunozcoben16@gmail.com>
2026-05-18 19:42:28 -0500

    actualizacion

--- consultorio/src/main/java/com/salud/consultorio/dto/citaMedica/DoctorRefCitaMedicaDTO.java
+++ consultorio/src/main/java/com/salud/consultorio/dto/citaMedica/DoctorRefCitaMedicaDTO.java
@@ -1,5 +1,6 @@
 package com.salud.consultorio.dto.citaMedica;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,9 +11,14 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@Schema(description = "DTO de referencia del doctor para la cita médica")
 public class DoctorRefCitaMedicaDTO {
 
     @NotNull(message = "El id del doctor es obligatorio")
+    @Schema(
+            description = "Identificador único del doctor",
+            example = "3"
+    )
     private Integer id;
 
 }
