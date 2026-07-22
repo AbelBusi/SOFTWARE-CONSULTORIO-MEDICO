d735b73db7e3333f608bb8a1a594ef988aefcea8
AbelBusi <cesarabelbustamante@gmail.com>
2026-07-22 12:01:58 -0500

    feat: Correccion de archivos del proyecto backend

--- app-front-consultorio/src/app/features/auth/models/auth.models.ts
+++
@@ -1,9 +0,0 @@
-export interface InicioSolicitud {
-  usuario: string;
-  claveAcceso: string;
-}
-
-export interface TokenResponse {
-  access_token: string;
-  refresh_token: string;
-}
--- app-front-consultorio/src/app/shared/components/empty-page/empty-page.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-empty-page',
-  imports: [],
-  templateUrl: './empty-page.html',
-  styleUrl: './empty-page.css',
-})
-export class EmptyPage {}
